255db74bec7390709ce2988c5acebfdfea07dae9
Gerardo Gimenez <geragimenez_96@hotmail.com>
2019-11-15 20:08:30 -0300

    Refactor reserve and release stock feature

--- src/rabbit/stockService.ts
+++ src/rabbit/stockService.ts
@@ -6,10 +6,8 @@
 import { RabbitDirectConsumer } from "./tools/directConsumer";
 import { RabbitFanoutEmitter } from "./tools/fanoutEmitter";
 import { IRabbitMessage } from "./tools/common";
-import { Stock, IStock } from "../stock/schema";
-import { IStockResponse } from "../stock";
-import * as error from "../server/error";
 import * as articleDeleted from "../stock/articleDeleted";
+import * as cartArticles from "../stock/cartArticles";
 
 interface ICartUpdatedMessage {
   articleId: string;
@@ -29,7 +27,7 @@ export function init() {
 
 function processCartArticleAdd(rabbitMessage: IRabbitMessage) {
   const updatedArticle: ICartUpdatedMessage = rabbitMessage.message;
-  processCartMovement("add", updatedArticle.articleId, updatedArticle.amount).then(stock => {
+  cartArticles.processCartMovement("add", updatedArticle.articleId, updatedArticle.amount).then(stock => {
     console.log("Article stock reserved!");
     console.log(stock);
   }).catch(err => {
@@ -39,55 +37,12 @@ function processCartArticleAdd(rabbitMessage: IRabbitMessage) {
 
 function processCartArticleRemove(rabbitMessage: IRabbitMessage) {
   const updatedArticle: ICartUpdatedMessage = rabbitMessage.message;
-  processCartMovement("remove", updatedArticle.articleId, updatedArticle.amount).then(stock => {
+  cartArticles.processCartMovement("remove", updatedArticle.articleId, updatedArticle.amount).then(stock => {
     console.log("Article stock released!");
     console.log(stock);
   }).catch(err => {
     console.error(err);
   });
-}
-
-async function processCartMovement(cartMovement: "add" | "remove", articleId: string, amount: number) {
-  const action = cartMovement === "add" ? "decrease" : "increase";
-  try {
-    return new Promise((resolve, reject) => {
-      Stock.findOne({
-        articleId: articleId,
-        enabled: true
-      }, function (err: any, stock: IStock) {
-        if (err) return reject(err);
-
-        if (!stock) {
-          const result = error.newError(error.ERROR_BAD_REQUEST, "Invalid article id");
-          reject(result);
-        } else {
-          if (stock.stock < amount) {
-            const result = error.newError(error.ERROR_BAD_REQUEST, "Invalid amount");
-            return reject(result);
-          }
-          stock.updateStock(action, amount);
-
-          // Save the Stock
-          stock.save(function (err: any, stock) {
-            if (err) return reject(err);
-
-            const result: IStockResponse = {
-              _id: stock._id,
-              articleId: stock.articleId,
-              stock: stock.stock,
-              minStockWarning: stock.minStockWarning,
-              updated: stock.updated,
-              created: stock.created,
-              enabled: stock.enabled,
-            };
-            resolve(result);
-          });
-        }
-      });
-    });
-  } catch (err) {
-    return Promise.reject(err);
-  }
 }
 
 interface ICatalogArticleDeletedMessage {
---
+++ src/stock/cartArticles.ts
@@ -0,0 +1,49 @@
+"use strict";
+
+import { Stock, IStock } from "../stock/schema";
+import { IStockResponse } from "../stock";
+import * as error from "../server/error";
+
+
+export async function processCartMovement(cartMovement: "add" | "remove", articleId: string, amount: number) {
+  const action = cartMovement === "add" ? "decrease" : "increase";
+  try {
+    return new Promise((resolve, reject) => {
+      Stock.findOne({
+        articleId: articleId,
+        enabled: true
+      }, function (err: any, stock: IStock) {
+        if (err) return reject(err);
+
+        if (!stock) {
+          const result = error.newError(error.ERROR_BAD_REQUEST, "Invalid article id");
+          reject(result);
+        } else {
+          if (stock.stock < amount) {
+            const result = error.newError(error.ERROR_BAD_REQUEST, "Invalid amount");
+            return reject(result);
+          }
+          stock.updateStock(action, amount);
+
+          // Save the Stock
+          stock.save(function (err: any, stock) {
+            if (err) return reject(err);
+
+            const result: IStockResponse = {
+              _id: stock._id,
+              articleId: stock.articleId,
+              stock: stock.stock,
+              minStockWarning: stock.minStockWarning,
+              updated: stock.updated,
+              created: stock.created,
+              enabled: stock.enabled,
+            };
+            resolve(result);
+          });
+        }
+      });
+    });
+  } catch (err) {
+    return Promise.reject(err);
+  }
+}
